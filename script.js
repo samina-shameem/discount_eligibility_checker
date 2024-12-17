60de11edcfdf896c3dcbbe815b6cb3113ba9a3bf
samina-shameem <samina.shameem.clt@gmail.com>
2024-12-17 17:44:10 +0100

    added extra if for button working. link to js.

--- script.js
+++ script.js
@@ -1,25 +1,30 @@
 //dom elements
-const ageInput=document.getElementById('age');
-const amountInput=document.getElementById('amount');
-const checkButton=document.getElementById('checkDisount');
-const result=document.getElementById('result');
+const ageInput = document.getElementById('age');
+const amountInput = document.getElementById('amount');
+const checkButton = document.getElementById('checkDiscount');
+const result = document.getElementById('result');
 
 //event listner
+if (checkButton) {
 
-checkButton.addEventListener('click' ,() => {
+    checkButton.addEventListener('click', () => {
+        console.log('Button clicked');
 
-//variables and data types
-const age=parseInt(ageInput.ariaValueMax, 10);//number
-const amount=parseInt(amountInput.value);//number
+        //variables and data types
+        const age = parseInt(ageInput.ariaValueMax, 10);//number
+        const amount = parseInt(amountInput.value);//number
 
-//"OR" operator logic
-if(age <18 || age >60 || amount> 1000) {
-    result.textContent="Congaratulations! You qualify for a discount.";
-    result.style.color='green';
-}else {
-    result.textContent="Sorry, you do not quality for a discount.";
-    result.style.color='red';
+        //"OR" operator logic
+        if (age < 18 || age > 60 || amount > 1000) {
+            result.textContent = "Congaratulations! You qualify for a discount.";
+            result.style.color = 'green';
+        } else {
+            result.textContent = "Sorry, you do not quality for a discount.";
+            result.style.color = 'red';
+        }
+
+
+    });
+} else {
+    console.error("Button with ID 'checkDiscount'not found.");
 }
-
-
-});
